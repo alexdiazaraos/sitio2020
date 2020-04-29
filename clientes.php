fc77d637b40071913e4a145506e4f94cda5fb9e6
lexdiazaraos <alexdiazaraos@gmail.com>
2020-04-28 22:31:57 -0400

    primera etapa agregar bootstrap

---
+++ clientes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-6 offset-3">
+                <div class="card rounded shadow">
+                    <div class="card-header">Registro de Clientes</div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="run"></label>
+                                <input type="text" name="run" id="run" class="form-control">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">Taller de Integración de Software 2020</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js\jquery-3.5.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
