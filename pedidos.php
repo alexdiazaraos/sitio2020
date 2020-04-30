c8c695df791c8abd5cdc120e2ec5188cfd5a1873
Alex Diaz Araos - INACAP <macbook@MacBook-Pro-de-MacBook.local>
2020-04-30 10:07:26 -0400

    Ete es un nuevo titulo

--- pedidos.php
+++ pedidos.php
@@ -12,6 +12,7 @@
     <div class="card-header">Registro de Pedidos</div>
     <div class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at esse perferendis quia asperiores. Quaerat, commodi porro incidunt, voluptatibus animi assumenda voluptatem quis quas culpa eos dolor nostrum unde quidem?</div>
     <div class="card-footer">invento</div>
+    <h3>Este es un titulo</h3>
     </div>
     </div>
     </div>
