a78fcd6a930b23312e19c52ea701ad53132d09b1
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2020-04-30 09:57:38 -0400

    agregando primera parte a pedidos

--- pedidos.php
+++ pedidos.php
@@ -6,6 +6,14 @@
     <title>pedidos</title>
 </head>
 <body>
-    
+    <div class="container">
+    <div class="row">
+    <div class="col-6 card">
+    <div class="card-header">Registro de Pedidos</div>
+    <div class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at esse perferendis quia asperiores. Quaerat, commodi porro incidunt, voluptatibus animi assumenda voluptatem quis quas culpa eos dolor nostrum unde quidem?</div>
+    <div class="card-footer">invento</div>
+    </div>
+    </div>
+    </div>
 </body>
 </html>
